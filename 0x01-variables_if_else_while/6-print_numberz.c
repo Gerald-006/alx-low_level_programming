b4c8d361177da35f40e2b6d02698694aa623a73b
Gerald-006 <jacksongeraldobere@gmail.come>
2022-09-09 16:00:42 -0700

    first

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on Success
+*/
+
+int main(void)
+
+{
+
+	int ascii = 48;
+
+
+
+	for (; ascii < 58; ascii++)
+
+	{
+
+		putchar(ascii);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on Success
+ */
+
+int main(void)
+
+{
+
+	char letter = 'z';
+
+
+
+	for (; letter >= 'a'; letter--)
+
+	{
+
+		putchar(letter);
+
+		if (letter == 'a')
+
+			putchar('\n');
+
+	}
+
+	return (0);
+
+}
