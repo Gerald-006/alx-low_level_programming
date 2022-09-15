972caea19cb01588d9312754608f21391499cc8a
Gerald-006 <jacksongeraldobere@gmail.come>
2022-09-15 12:54:22 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: counter
+ * Return: nothing.
+ */
+
+void print_line(int n)
+
+{
+
+	int i;
+
+
+
+	for (i = 0; i < n; i++)
+
+	{
+
+		_putchar(95);
+
+	}
+
+	_putchar('\n');
+
+}
